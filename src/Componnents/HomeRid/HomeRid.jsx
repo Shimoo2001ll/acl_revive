262abd4de6bf6feb42afe98c935c8b9254d2f9da
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2024-02-27 00:26:45 +0200

    styling

--- src/Componnents/HomeRid/HomeRid.jsx
+++ src/Componnents/HomeRid/HomeRid.jsx
@@ -7,12 +7,12 @@ import aclLegknee from '../../asset/Leg with knee pain.svg';
 const HomeRid = () => {
   return (
     <div className='mHome '>
-            <div className='intro  container mt-1 '>
+            <div className='intro   mt-1 '>
                 <div className='introtitle w-50 '>
                     <img src={acl} alt='acl-Head' />
                     <p className='text-muted w-100 '>crucial step in regaining full knee function after an injury to this important ligament. The rehabilitation process typically consists of four phases:</p>
                 </div>
-                <div className='imgIntro d-none d-md-block d-lg-block d-xl-block d-xxl-block '>
+                <div className='imgIntro text-center  d-none d-md-block d-lg-block d-xl-block d-xxl-block '>
                     <img src={aclLegknee} alt="aclLegknee" title='acl Leg knee with pain' />
                 </div>
             </div>
