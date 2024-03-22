d7c6bd8d1919665f1da41ef454f9521705a0ba8f
ALshimaa jamal <89547012+Shimoo2001ll@users.noreply.github.com>
2024-03-22 15:05:35 +0200

    validation
    add new validation in login page and finally I finish it

--- src/Componnents/Login/Login.jsx
+++ src/Componnents/Login/Login.jsx
@@ -6,13 +6,15 @@ import { Link, useNavigate } from 'react-router-dom';
 import './Login.css'
 import { FaRegEnvelope } from 'react-icons/fa'
 import { FaLock } from 'react-icons/fa'
-import toast, { Toaster, } from 'react-hot-toast';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+// import { ToastContainer } from 'react-bootstrap';
 
 function Login() {
     const [email, setEmail] = useState('')
     const [pass, setPass] = useState('')
-    const [error, setErorr] = useState(false)
-    const [alert, setAlert] = useState(false)
+    // const [error, setErorr] = useState(false)
+    // const [alert, setAlert] = useState(false)
 
 
     const navigate = useNavigate()
@@ -21,26 +23,36 @@ function Login() {
     const checkInputs = () => {
         // Regular expression for email validation
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    
+
         // Check if email and password are not empty
         if (email === "" || pass === "") {
-            return false;
+            toast.error('Please fill in all fields')
+            return;
         }
-    
+
         // Check if email is in valid format
         if (!emailRegex.test(email)) {
-            return false;
+            toast.error('Please enter valid mail contain (@ , .com ) ')
+            return;
+        } else {
+            // setAlert('')
         }
-    
+
         // Check if password length is at least 6 characters
-        if (pass.length < 6) {
-            return false;
+        if (pass.length < 8) {
+            toast.error('Passward must be at least 8 characters long')
+            return;
         }
-    
-        return true;
+        //  if all vaildation pass , process with success
+        toast.success("You are loggied in !")
+        setTimeout(() => {
+            navigate("/HomeRid")
+        }, 1000)
+        return;
     }
 
     const handelChangeEmail = (e) => {
+
         setEmail(e.target.value)
 
     }
@@ -48,65 +60,54 @@ function Login() {
         setPass(e.target.value)
     }
 
-    const myToast = () => {
-        if (checkInputs()) {
-            toast.success("You are loggied in !")
-            setTimeout(()=> {
-                navigate("/HomeRid")
-            },1000)
-        } else {
-            toast.error("Please Fill all the inputs!")
-        }
-    }
 
+        return (
+            <div className=' log  w-75 mt-2 pt-5 '>
 
-    return (
-        <div className=' log  w-75 mt-2 pt-5 '>
-
-            <div className='m-5 text-center'>
-                <img src={Logo} alt="acl logo" title='Acl logo' width={100} height={100} />
-            </div>
-            <div className='form w-75'>
-                <form action="" className='' >
-                    <div className='mb-3 w-100'>
-                        <label className="form-label fw-bold">Email : </label>
-                        <div className='inp border border-muted row p-2 rounded'>
-                            <FaRegEnvelope className='awenvelop col-2 p-0 m-0 ' />
-                            <input type='email' placeholder='example@gmail.com' className=' col-10 m-0 p-0' value={email} onChange={handelChangeEmail} />
+                <div className='m-5 text-center'>
+                    <img src={Logo} alt="acl logo" title='Acl logo' width={100} height={100} />
+                </div>
+                <div className='form w-75'>
+                    <form action="" className='' >
+                        <div className='mb-3 w-100'>
+                            <label className="form-label fw-bold">Email : </label>
+                            <div className='inp border border-muted row p-2 rounded'>
+                                <FaRegEnvelope className='awenvelop col-2 p-0 m-0 ' />
+                                <input type='email' placeholder='example@gmail.com' className=' col-10 m-0 p-0' value={email} onChange={handelChangeEmail} />
+                            </div>
                         </div>
-                    </div>
-                    {alert && setAlert(<p className='alert alert-danger  m-2'> Email should contain @  </p>)}
-                    <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label fw-bold ">Password:</label>
-                        <div className='inp border border-muted row p-2 rounded'>
-                            <FaLock className='awenvelop col-2 p-0 m-0 ' />
-                            <input type="password" placeholder='****************' className=' col-10 m-0 p-0' value={pass} onChange={handelChangePass} />
+                        {/* {alert} */}
+                        <div className="mb-3">
+                            <label htmlFor="exampleInputPassword1" className="form-label fw-bold ">Password:</label>
+                            <div className='inp border border-muted row p-2 rounded'>
+                                <FaLock className='awenvelop col-2 p-0 m-0 ' />
+                                <input type="password" placeholder='****************' className=' col-10 m-0 p-0' value={pass} onChange={handelChangePass} />
+                            </div>
                         </div>
-                    </div>
-                    <div>
-                        <Link type="submit" onClick={() => myToast()} className="btn  w-100 text-center text-capitalize "
-                        > log in</Link>
-                    </div>
-                </form>
-                {error && <p className=' alert  alert-danger  text-capitalize m-3  '> Please fill in all the fielde </p>}
-            </div>
-            <div className=' mt-5'>
-                <Link to="/forget" className='forgetPass text-muted fs-6 '> Forget Password?</Link>
-            </div>
-            <div className='OR text-center m-3 p-3'>
-                <div className='otherLink text-muted text-capitalize p-2 m-2'>or</div>
-                <div className=" awsom text-center">
-                    <div className='goo  text-center '>
-                        <img src={google} alt="" />
-                    </div>
-                    <div className='face  text-center '>
-                        <img src={facebook} alt="" />
+                        <div>
+                            <Link type="submit" onClick={checkInputs} className="btn  w-100 text-center text-capitalize "
+                            > log in</Link>
+                        </div>
+                    </form>
+
+                </div>
+                <div className=' mt-5'>
+                    <Link to="/forget" className='forgetPass text-muted fs-6 '> Forget Password?</Link>
+                </div>
+                <div className='OR text-center m-3 p-3'>
+                    <div className='otherLink text-muted text-capitalize p-2 m-2'>or</div>
+                    <div className=" awsom text-center">
+                        <div className='goo  text-center '>
+                            <img src={google} alt="" />
+                        </div>
+                        <div className='face  text-center '>
+                            <img src={facebook} alt="" />
+                        </div>
                     </div>
                 </div>
+                <ToastContainer />
             </div>
-            <Toaster />
-        </div>
-    )
-}
+        )
+    }
 
-export default Login;
+    export default Login;
